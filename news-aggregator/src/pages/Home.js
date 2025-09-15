bab9eee32431099f0603f5b5e2290cb710b1dab0
duckhead013 <happiestfillsom@gmail.com>
2025-09-15 16:02:22 +0530

    update

--- news-aggregator/src/pages/Home.js
+++ news-aggregator/src/pages/Home.js
@@ -2,4 +2,4 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NewsCard from '../components/NewsCard';
 import { auth, db } from '../firebase';
-import { doc}
+import { doc};okokokok
